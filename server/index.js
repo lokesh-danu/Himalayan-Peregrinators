d42e4f3442aeb2c85cc64494cca9069efe8156b5
lokesh-danu <lokeshdanu9@gmail.com>
2023-03-14 12:48:47 +0530

    restrucutre

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const emailRoute= require('./routes/email');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
+
+/*
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -26,7 +28,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
+*/
 app.use('/api/email',emailRoute);
 
 app.listen(Port, () => {
